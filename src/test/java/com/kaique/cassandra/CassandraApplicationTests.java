b8b24417e44aec609994180d8e4ba7934f119da6
kaiquesilvadev <j.kaique1570@gmail.com>
2024-03-21 19:00:02 -0300

    first commit

---
+++ src/test/java/com/kaique/cassandra/CassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaique.cassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
