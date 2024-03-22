d81d82b286f1af59289b430e6df65bf342ad938b
kaiquesilvadev <j.kaique1570@gmail.com>
2024-03-22 18:41:04 -0300

    GET Department by id

--- src/main/java/com/kaique/cassandra/services/DepartmentService.java
+++ src/main/java/com/kaique/cassandra/services/DepartmentService.java
@@ -1,6 +1,8 @@
 package com.kaique.cassandra.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +21,15 @@ public class DepartmentService {
 	public List<DepartmentDTO> findAll() {
 		List<Department> list = repository.findAll();
 		return list.stream().map(x -> new DepartmentDTO(x)).collect(Collectors.toList());
+	}
+	
+	private Department getById(UUID id) {
+		Optional<Department> result = repository.findById(id);
+		return result.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado"));
+	}
+	
+	public DepartmentDTO findById(UUID id) {
+		Department entity = getById(id);
+		return new DepartmentDTO(entity);
 	}
 }	
