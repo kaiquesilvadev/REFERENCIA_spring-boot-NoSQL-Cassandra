17bc7a06c6733e188a3fcd92e474080d6f0a3d8f
kaiquesilvadev <j.kaique1570@gmail.com>
2024-03-22 18:59:54 -0300

    GET Product by id

---
+++ src/main/java/com/kaique/cassandra/controlles/ProductController.java
@@ -0,0 +1,24 @@
+package com.kaique.cassandra.controlles;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+	@Autowired
+	private ProductService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> findById(@PathVariable UUID id) {
+		ProductDTO obj = service.findById(id);
+		return ResponseEntity.ok(obj);
+	}
+}
