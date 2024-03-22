08df5d3bfbd9e94cf4aade4c16873ad2118f17a9
kaiquesilvadev <j.kaique1570@gmail.com>
2024-03-22 19:14:47 -0300

    GET Products by department name

--- src/main/java/com/kaique/cassandra/repository/ProductRepository.java
+++ src/main/java/com/kaique/cassandra/repository/ProductRepository.java
@@ -1,10 +1,16 @@
 package com.kaique.cassandra.repository;
 
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
 
 import com.kaique.cassandra.model.entities.Product;
 
 public interface ProductRepository extends CassandraRepository<Product, UUID> {
+	
+	@AllowFiltering
+	List<Product> findByDepartment(String department);
 }
